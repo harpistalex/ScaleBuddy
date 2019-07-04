c9694802c728f9182bcb925e3071fdaeea75160e
Simon Bennett <simon@traxsound.co.uk>
2019-07-04 11:11:08 +0100

    Initial Commit

---
+++ ScaleBuddy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScaleBuddy
+//
+//  Created by Alexandra King on 04/07/2019.
+//  Copyright © 2019 Alex's Amazing Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
